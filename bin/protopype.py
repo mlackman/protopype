2ce50288fe92786d8e9244a7f173de8d12d04c15
Mika Lackman <mika.lackman@gmail.com>
2016-11-11 17:55:15 +0200

    cors header

--- bin/protopype.py
+++ bin/protopype.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 from bottle import static_file, route, run
 
+def serve_static_file(filename, root):
+  response = static_file(filename, root)
+  response.set_header('Access-Control-Allow-Origin', '*')
+  return response
 
 @route('/')
 def serve_html():
-  return static_file('index.html', root='')
+  return serve_static_file('index.html', root='')
 
 @route('/<filename:path>')
 def serve(filename):
-  return static_file(filename, root='')
+  return serve_static_file(filename, root='')
 
 if __name__ == '__main__':
   run(host='0.0.0.0', port=3000, debug=True, reload=True)
